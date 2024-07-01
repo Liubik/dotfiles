bd4d645a7384d4e5bdd4a642c90d4aaeac391db7
Calowed <40126388+Liubik@users.noreply.github.com>
2024-06-30 18:43:37 -0700

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,9 +6,10 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
-#Delay
-sleep 3
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Bars launched..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload example &
+fi
